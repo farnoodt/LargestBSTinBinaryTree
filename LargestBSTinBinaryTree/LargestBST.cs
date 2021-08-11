9d3f7bd741da75126f7d6a3e5a8c360679e2fc69
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-08-11 00:36:18 -0700

    Add project files.

---
+++ LargestBSTinBinaryTree/LargestBST.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestBSTinBinaryTree
+{
+    class LargestBST
+    {
+        public int FindlargestBST(Node root)
+        {
+            MinMax m = Largest(root);
+            return m.Size;
+        }
+        private MinMax Largest(Node root)
+        {
+            if (root == null)
+                return new MinMax();
+
+            MinMax leftMinMax = Largest(root.left);
+            MinMax rightMinMax = Largest(root.right);
+
+            MinMax M = new MinMax();
+
+            if(leftMinMax.IsBTS == false || rightMinMax.IsBTS == false || root.data < leftMinMax.Max || root.data >= rightMinMax.Min)
+            {
+                M.IsBTS = false;
+                M.Size = Math.Max(leftMinMax.Size, rightMinMax.Size);
+                return M;
+            }
+
+            M.Size = leftMinMax.Size + rightMinMax.Size + 1;
+            M.IsBTS = true;
+            M.Min = root.left != null ? root.left.data : root.data;
+            M.Max = root.right != null ? root.right.data : root.data;
+
+            return M;
+        }
+
+        
+    }
+
+    class MinMax
+    {
+        public int Min;
+        public int Max;
+        public int Size;
+        public bool IsBTS;
+
+        public MinMax()
+        {
+            this.Max = int.MinValue;
+            this.Min = int.MaxValue;
+            this.IsBTS = true;
+            this.Size = 0;
+        }
+    }
+}
---
+++ LargestBSTinBinaryTree/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LargestBSTinBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node tree = new Node(25);
+            tree.left = new Node(18);
+            tree.left.left = new Node(19);
+            tree.left.left.right = new Node(15);
+            tree.left.right = new Node(20);
+            tree.left.right.right = new Node(25);
+            tree.left.right.left = new Node(18);
+
+            tree.right = new Node(50);
+            tree.right.right = new Node(60);
+            tree.right.right.right = new Node(70);
+            tree.right.right.left = new Node(55);
+            tree.right.left = new Node(35);
+            tree.right.left.left = new Node(20);
+            tree.right.left.left.right = new Node(25);
+            tree.right.left.right = new Node(40);
+
+            LargestBST LB = new LargestBST();
+
+            Console.WriteLine(LB.FindlargestBST(tree));
+            Console.ReadLine();
+        }
+    }
+}
